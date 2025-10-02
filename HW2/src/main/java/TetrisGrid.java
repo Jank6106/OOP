7ceb6e53239320f3cb8e2ecbeeeb2509336c9956
Jank6106 <24020058@vnu.edu.vn>
2025-10-03 04:32:30 +0700

    HW2

--- HW2/src/main/java/TetrisGrid.java
+++ HW2/src/main/java/TetrisGrid.java
@@ -18,9 +18,36 @@ public class TetrisGrid {
 	 * Does row-clearing on the grid (see handout).
 	 */
 	public void clearRows() {
-        
+        int width = grid.length;
+        int heigh = grid[0].length;
+        int currentRowAfterDeleted = 0;
+
+        for (int i = 0; i < heigh; i++) {
+            if (!rowIsFullTrue(i)) {
+                for (int j = 0; j < width; j++) {
+                    grid[j][currentRowAfterDeleted] = grid[j][i];
+                }
+                currentRowAfterDeleted++;
+            }
+        }
+
+        for (int i = currentRowAfterDeleted; i < heigh; i++) {
+            for (int j = 0; j < width; j++) {
+                grid[j][i] = false;
+            }
+        }
 	}
-	
+
+    private boolean rowIsFullTrue(int indexCol) {
+        int width = grid.length;
+        for (int i = 0; i < width; i++) {
+            if (grid[i][indexCol] == false) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 	/**
 	 * Returns the internal 2d grid array.
 	 * @return 2d grid array
